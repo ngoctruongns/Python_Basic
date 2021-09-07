dbd141309325ffcc02ebba9de39de2a2a8de6fce
ngoctruongns <vungoctruong8x@gmail.com>
2021-09-07 21:48:49 +0700

    Tao file dau tien

---
+++ Test_02.py
@@ -0,0 +1,21 @@
+print('Hello')
+print('File test 2')
+# for <bien_dem> in range(min,max,step):  step > 0
+print('step > 0')
+for i in range(1,9,3):
+  print(i)
+# for <bien_dem> in range(min,max,step):  step < 0
+print('step < 0')
+for i in range(9,1,-3):
+  print(i)
+# for <bien_dem> in range(Max):
+print('max')
+for i in range(6):
+    print(i) 
+print('the end')
+# While <dieu_kien> :
+m = 4
+while m>0 :
+    print(f'm = {m}')
+    m -=1
+print('Ket Thuc')
