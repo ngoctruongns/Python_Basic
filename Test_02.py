1af6ac3de7e431d1d4ea0e79a6eeb71a20cebb26
ngoctruongns <vungoctruong8x@gmail.com>
2021-09-07 16:52:31 +0700

    Cac loai vong lap For

--- Test_02.py
+++ Test_02.py
@@ -1,6 +1,15 @@
 print('Hello')
 print('File test 2')
+# for <bien_dem> in range(min,max,step):  step > 0
+print('step > 0')
 for i in range(1,9,3):
   print(i)
+# for <bien_dem> in range(min,max,step):  step < 0
+print('step < 0')
+for i in range(9,1,-3):
+  print(i)
+# for <bien_dem> in range(Max):
+print('max')
+for i in range(6):
+    print(i) 
 print('the end')
-
